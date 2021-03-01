bf575e13a378d5ab2db5a867e4ef8910b0d1c7ba
Lloyd Rochester <lloyd@lloydrochester.com>
2021-03-01 08:52:36 -0700

    started refactor

--- src/clip.c
+++ src/clip.c
@@ -1,1 +1,56 @@
 #include "clip.h"
+
+int
+clip_init(struct Clip *clip)
+{
+  clip->prev_recording = false;
+  clip->recording = false;
+  clip->sample_counter = 0L;
+  return 0;
+}
+
+bool
+clip_under_threshold(struct Clip *clip, int16_t pcm_buffer[], size_t pcm_buffer_size)
+{
+  int16_t pcm_value;
+  bool under_threshold = true;
+  for(int i=0; i<pcm_buffer_size; i++)
+  {
+    pcm_value = pcm_buffer[i];
+    bool meets_thresh = pcm_value > clip->thresh_pos || pcm_value < clip->thresh_neg;
+    if(meets_thresh)
+    {
+      under_threshold = false;
+      clip->thresh_current.t = clip->sample_counter+i;
+      clip->thresh_current.y = pcm_value;
+      if(clip->recording == 0)
+      {
+        clip->thresh_first.t = clip->thresh_current.t;
+        clip->thresh_first.y = pcm_value;
+        clip->recording = true;
+        clip->prev_recording = false;
+        clip->thresh_first_time = time(NULL);
+      }
+      else
+        clip->prev_recording = true;
+    }
+  }
+  clip->sample_counter += pcm_buffer_size;
+  return under_threshold;
+}
+
+int
+clip_keep_recording(struct Clip *clip)
+{
+  assert(clip->recording == true);
+  assert(clip->sample_counter > clip->thresh_current.t);
+
+  unsigned long diff = clip->sample_counter - clip->thresh_current.t;
+
+  // TODO need to set from options
+  if(diff > 16000)
+  {
+    return 0;
+  }
+  return 1;
+}
--- src/clip.h
+++ src/clip.h
@@ -1,7 +1,10 @@
 #ifndef CLIP_H
 #define CLIP_H
 
+#include <assert.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 
 struct Sample
 {
@@ -11,10 +14,26 @@ struct Sample
 
 struct Clip
 {
-  int recording;
+  bool prev_recording;
+  bool recording;
   int16_t thresh_pos, thresh_neg;
-  struct Sample thresh_first;
-  struct Sample thresh_current;
+  struct Sample thresh_first, thresh_current;
+  time_t thresh_first_time;
+  unsigned long sample_counter;
 };
+
+int
+clip_init(struct Clip *clip);
+
+bool
+clip_under_threshold(struct Clip *clip, int16_t pcm_buffer[], size_t pcm_buffer_size);
+
+int
+clip_keep_recording(struct Clip *clip);
+
+/*
+int
+clip_generate_wavfile(struct Clip *clip, char *pcm_buffer_first, 
+*/
 
 #endif
